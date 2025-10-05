cd72a35721e0a30cde7ad327d546fb64ddcedec7
Outro-Lucas <lucasferreirapacoti@gmail.com>
2025-10-05 18:45:35 -0300

    feat: Alterando tema

--- src/app/components/navhome/navhome.ts
+++ src/app/components/navhome/navhome.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+// import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navhome',
-  imports: [RouterLink],
+  // imports: [RouterLink],
   templateUrl: './navhome.html',
   styleUrl: './navhome.css'
 })
