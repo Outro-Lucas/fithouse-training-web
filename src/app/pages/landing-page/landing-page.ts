e57c24118d80a3792d2b014b7533988f1d4af709
Outro-Lucas <lucasferreirapacoti@gmail.com>
2025-10-06 19:16:38 -0300

    feat: Beneficios

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Navhome } from '../../components/navhome/navhome';
 import { Hero } from '../../components/hero/hero';
 import { CardCarousel } from '../../components/card-carousel/card-carousel';
+import { Features } from "../../components/features/features";
 
 @Component({
   selector: 'app-landing-page',
@@ -9,7 +10,8 @@ import { CardCarousel } from '../../components/card-carousel/card-carousel';
     Navhome,
     Hero,
     CardCarousel,
-  ],
+    Features
+],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css'
 })
