770a8cb826f83f31a8fdd7e5487d756e522ac3c0
Outro-Lucas <lucasferreirapacoti@gmail.com>
2025-10-07 14:27:52 -0300

    feat: Scroll via navbar

--- src/app/components/navhome/navhome.ts
+++ src/app/components/navhome/navhome.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-// import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navhome',
-  // imports: [RouterLink],
   templateUrl: './navhome.html',
   styleUrl: './navhome.css'
 })
 export class Navhome {
 
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      // Calcula a posição considerando a navbar fixa
+      const navbarHeight = 80; // Aproximadamente a altura da sua navbar
+      const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition - navbarHeight;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
