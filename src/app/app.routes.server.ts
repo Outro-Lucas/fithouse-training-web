991aec110ebc3f17e43ce85e1d3e971eac402d2a
Outro-Lucas <lucasferreirapacoti@gmail.com>
2026-02-20 10:36:45 -0300

    feat: Configurando SSR novamente

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },     // Landing (SSG)
+  { path: 'auth', renderMode: RenderMode.Client },    // login: client-side (ou Server se preferir)
+  { path: 'treino', renderMode: RenderMode.Client },  // por enquanto client (mudar p/ Server se precisar ler cookie)
+  { path: 'inicio', renderMode: RenderMode.Client },
+  { path: 'categorias', renderMode: RenderMode.Client },
+  { path: 'exercicios', renderMode: RenderMode.Client },
+  // fallback — caso queira prerender tudo o resto:
+  { path: '**', renderMode: RenderMode.Prerender }
 ];
